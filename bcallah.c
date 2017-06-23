598ab77dd74bc5c4548ace6117ee63f795410320
Brian Callahan <dodonpachi-github@mailinator.com>
2017-06-23 09:50:37 -0400

    More needless obfuscation.

--- bcallah.c
+++ bcallah.c
@@ -1,23 +1,16 @@
 /* Public Domain */
 #include <stdio.h>
-static char c[3];int main(void){char*b=c;++*b;++*b;++*b;++*b;++*b;++*b;
-++*b;++*b;++*b;while(*b){++b;++*b;++*b;++*b;++*b;++*b;++*b;++*b;++*b;
-++*b;++*b;++*b;--b;--*b;}++b;--*b;putchar(*b);++*b;putchar(*b);--*b;--*b;
-putchar(*b);--b;++*b;++*b;++*b;++*b;while(*b){++b;++*b;++*b;++*b;--b;
---*b;}++b;--*b;putchar(*b);putchar(*b);--b;++*b;++*b;++*b;++*b;while(*b){
-++b;--*b;--*b;--*b;--b;--*b;}++b;++*b;putchar(*b);--b;++*b;++*b;while(*b)
-{++b;++*b;++*b;++*b;--b;--*b;}++b;++*b;putchar(*b);--b;++*b;++*b;++*b;
-++*b;++*b;++*b;++*b;++*b;while(*b){++b;++b;++*b;++*b;++*b;++*b;++*b;++*b;
-++*b;++*b;--b;--b;--*b;}++b;++b;putchar(*b);--b;++*b;++*b;++*b;++*b;++*b;
-++*b;++*b;putchar(*b);++*b;putchar(*b);--b;++*b;++*b;++*b;while(*b){++b;
---*b;--*b;--*b;--b;--*b;}++b;--*b;--*b;putchar(*b);--b;++*b;++*b;++*b;
-while(*b){++b;++*b;++*b;++*b;--b;--*b;}++b;putchar(*b);--b;++*b;++*b;++*b
-;while(*b){++b;--*b;--*b;--*b;--*b;--b;--*b;}++b;putchar(*b);--b;++*b;
-++*b;++*b;++*b;while(*b){++b;++*b;++*b;++*b;++*b;--b;--*b;}++b;++*b;
-putchar(*b);--b;++*b;++*b;++*b;while(*b){++b;--*b;--*b;--*b;--*b;--*b;--b
-;--*b;}++b;putchar(*b);--b;++*b;++*b;++*b;++*b;while(*b){++b;++b;--*b;
---*b;--*b;--*b;--b;--b;--*b;}++b;++b;--*b;--*b;putchar(*b);--b;--b;++*b;
-++*b;++*b;while(*b){++b;++*b;++*b;++*b;--b;--*b;}++b;++*b;++*b;putchar(*b
-);++*b;++*b;++*b;putchar(*b);--b;++*b;++*b;++*b;while(*b){++b;--*b;--*b;
---*b;--b;--*b;}++b;--*b;--*b;putchar(*b);--b;++*b;++*b;++*b;++*b;++*b;
-++*b;++*b;++*b;++*b;++*b;putchar(*b);return 0;}
+#define d putchar(*b)
+#define p while(*b){
+#define q ++*b
+#define u --*b
+static char c[3];int main(void){char*b=c;q;q;q;q;q;q;q;q;q;p++b;q;q;q;q;q
+;q;q;q;q;q;q;--b;u;}++b;u;d;q;d;u;u;d;--b;q;q;q;q;p++b;q;q;q;--b;u;}++b;u
+;d;d;--b;q;q;q;q;p++b;u;u;u;--b;u;}++b;q;d;--b;q;q;p++b;q;q;q;--b;u;}++b;
+q;d;--b;q;q;q;q;q;q;q;q;p++b;++b;q;q;q;q;q;q;q;q;--b;--b;u;}++b;++b;d;--b
+;q;q;q;q;q;q;q;d;q;d;--b;q;q;q;p++b;u;u;u;--b;u;}++b;u;u;d;--b;q;q;q;p++b
+;q;q;q;--b;u;}++b;d;--b;q;q;q;p++b;u;u;u;u;--b;u;}++b;d;--b;q;q;q;q;p++b;
+q;q;q;q;--b;u;}++b;q;d;--b;q;q;q;p++b;u;u;u;u;u;--b;u;}++b;d;--b;q;q;q;q;
+p++b;++b;u;u;u;u;--b;--b;u;}++b;++b;u;u;d;--b;--b;q;q;q;p++b;q;q;q;--b;u;
+}++b;q;q;d;q;q;q;d;--b;q;q;q;p++b;u;u;u;--b;u;}++b;u;u;d;--b;q;q;q;q;q;q;
+q;q;q;q;d;return 0;}
