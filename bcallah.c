a10b07e3d53afc629a01680aa77689a248993379
Brian Callahan <dodonpachi-github@mailinator.com>
2017-06-28 03:09:57 -0400

    Only use letters in my name.

--- bcallah.c
+++ bcallah.c
@@ -1,15 +1,15 @@
 /* Public Domain */
 #include <unistd.h>
-#define b ++*p
-#define r write(1,p,1)
-#define i while(*p){++p
-#define a --*p
-#define n --p
-static char c[3];int main(void){char*p=c;b;b;b;b;b;b;b;b;b;i;b;b;b;b;b;b;
-b;b;b;b;b;n;a;}++p;a;r;b;r;a;a;r;n;b;b;b;b;i;b;b;b;n;a;}++p;a;r;r;n;b;b;b
-;b;i;a;a;a;n;a;}++p;b;r;n;b;b;i;b;b;b;n;a;}++p;b;r;n;b;b;b;b;b;b;b;b;i;++
-p;b;b;b;b;b;b;b;b;n;n;a;}++p;++p;r;n;b;b;b;b;b;b;b;r;b;r;n;b;b;b;i;a;a;a;
-n;a;}++p;a;a;r;n;b;b;b;i;b;b;b;n;a;}++p;r;n;b;b;b;i;a;a;a;a;n;a;}++p;r;n;
-b;b;b;b;i;b;b;b;b;n;a;}++p;b;r;n;b;b;b;i;a;a;a;a;a;n;a;}++p;r;n;b;b;b;b;i
-;++p;a;a;a;a;n;n;a;}++p;++p;a;a;r;n;n;b;b;b;i;b;b;b;n;a;}++p;b;b;r;b;b;b;
-r;n;b;b;b;i;a;a;a;n;a;}++p;a;a;r;n;b;b;b;b;b;b;b;b;b;b;r;return 0;}
+#define b ++*l
+#define r write(1,l,1)
+#define i while(*l){++l
+#define a --*l
+#define n --l
+static char c[3];int main(void){char*l=c;b;b;b;b;b;b;b;b;b;i;b;b;b;b;b;b;
+b;b;b;b;b;n;a;}++l;a;r;b;r;a;a;r;n;b;b;b;b;i;b;b;b;n;a;}++l;a;r;r;n;b;b;b
+;b;i;a;a;a;n;a;}++l;b;r;n;b;b;i;b;b;b;n;a;}++l;b;r;n;b;b;b;b;b;b;b;b;i;++
+l;b;b;b;b;b;b;b;b;n;n;a;}++l;++l;r;n;b;b;b;b;b;b;b;r;b;r;n;b;b;b;i;a;a;a;
+n;a;}++l;a;a;r;n;b;b;b;i;b;b;b;n;a;}++l;r;n;b;b;b;i;a;a;a;a;n;a;}++l;r;n;
+b;b;b;b;i;b;b;b;b;n;a;}++l;b;r;n;b;b;b;i;a;a;a;a;a;n;a;}++l;r;n;b;b;b;b;i
+;++l;a;a;a;a;n;n;a;}++l;++l;a;a;r;n;n;b;b;b;i;b;b;b;n;a;}++l;b;b;r;b;b;b;
+r;n;b;b;b;i;a;a;a;n;a;}++l;a;a;r;n;b;b;b;b;b;b;b;b;b;b;r;return 0;}
