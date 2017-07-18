eeb0f2935ef3e7cd48b839bdc7e2c423f56dad3b
Brian Callahan <bcallah@openbsd.org>
2017-07-18 00:37:38 -0400

    Save 4 bytes.

--- bcallah.c
+++ bcallah.c
@@ -5,11 +5,11 @@
 #define i while(*l){++l
 #define a --*l
 #define n --l
-static char c[3];int main(void){char*l=c;b;b;b;b;b;b;b;b;b;i;b;b;b;b;b;b;
-b;b;b;b;b;n;a;}++l;a;r;b;r;a;a;r;n;b;b;b;b;i;b;b;b;n;a;}++l;a;r;r;n;b;b;b
-;b;i;a;a;a;n;a;}++l;b;r;n;b;b;i;b;b;b;n;a;}++l;b;r;n;b;b;b;b;b;b;b;b;i;++
-l;b;b;b;b;b;b;b;b;n;n;a;}++l;++l;r;n;b;b;b;b;b;b;b;r;b;r;n;b;b;b;i;a;a;a;
-n;a;}++l;a;a;r;n;b;b;b;i;b;b;b;n;a;}++l;r;n;b;b;b;i;a;a;a;a;n;a;}++l;r;n;
-b;b;b;b;i;b;b;b;b;n;a;}++l;b;r;n;b;b;b;i;a;a;a;a;a;n;a;}++l;r;n;b;b;b;b;i
-;++l;a;a;a;a;n;n;a;}++l;++l;a;a;r;n;n;b;b;b;i;b;b;b;n;a;}++l;b;b;r;b;b;b;
-r;n;b;b;b;i;a;a;a;n;a;}++l;a;a;r;n;b;b;b;b;b;b;b;b;b;b;r;return 0;}
+static char c[3],*l=c;int main(void){b;b;b;b;b;b;b;b;b;i;b;b;b;b;b;b;b;b;
+b;b;b;n;a;}++l;a;r;b;r;a;a;r;n;b;b;b;b;i;b;b;b;n;a;}++l;a;r;r;n;b;b;b;b;i
+;a;a;a;n;a;}++l;b;r;n;b;b;i;b;b;b;n;a;}++l;b;r;n;b;b;b;b;b;b;b;b;i;++l;b;
+b;b;b;b;b;b;b;n;n;a;}++l;++l;r;n;b;b;b;b;b;b;b;r;b;r;n;b;b;b;i;a;a;a;n;a;
+}++l;a;a;r;n;b;b;b;i;b;b;b;n;a;}++l;r;n;b;b;b;i;a;a;a;a;n;a;}++l;r;n;b;b;
+b;b;i;b;b;b;b;n;a;}++l;b;r;n;b;b;b;i;a;a;a;a;a;n;a;}++l;r;n;b;b;b;b;i;++l
+;a;a;a;a;n;n;a;}++l;++l;a;a;r;n;n;b;b;b;i;b;b;b;n;a;}++l;b;b;r;b;b;b;r;n;
+b;b;b;i;a;a;a;n;a;}++l;a;a;r;n;b;b;b;b;b;b;b;b;b;b;r;return 0;}
